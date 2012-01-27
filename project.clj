def68c4d3e7f34f9c4f3fb6696f57d838cc4b8a6
Michael Locher <cmbntr@gmail.com>
2012-01-27 03:40:39 +0100

    prepare next iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cmbntr/cmbntr.args "1.0.1"
+(defproject cmbntr/cmbntr.args "1.1.0-SNAPSHOT"
   :description "commandline argument parsing (with commons-cli)"
   :url "https://github.com/cmbntr/cmbntr.args"
   :dependencies [[org.clojure/clojure "1.3.0"]
