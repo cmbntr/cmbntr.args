2aa44a3597be7e13095a8301461b53f36bfb22be
Michael Locher <cmbntr@gmail.com>
2014-01-26 21:47:32 +0100

    fix project.clj and bump version

--- project.clj
+++ project.clj
@@ -1,7 +1,18 @@
 (defproject cmbntr/cmbntr.args "1.1.0-SNAPSHOT"
+  :min-lein-version "2.0.0"
+
   :description "commandline argument parsing (with commons-cli)"
   :url "https://github.com/cmbntr/cmbntr.args"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :scm {:url "git@github.com:cmbntr/cmbntr.args.git"}
+  :pom-addition [:developers [:developer
+                              [:name "Michael Locher"]
+                              [:email "cmbntr@gmail.com"]
+                              [:timezone "1"]]]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [commons-cli "1.2"]]
   :aot [cmbntr.args.main]
   :main cmbntr.args.main)
+
